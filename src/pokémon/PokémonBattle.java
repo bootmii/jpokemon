6489fd49eef59096ee32095bcc36e5101c50fe41
David <David@192.168.1.98>
2015-10-01 16:49:10 -0700

    Added types, changed several external values to public

--- src/pokémon/PokémonBattle.java
+++ src/pokémon/PokémonBattle.java
@@ -9,7 +9,7 @@ public class PokémonBattle {
 	public int getDamage(PokémonMove move) {
 		// ((2*Level*10/250)*(Attack/Defense)*Base+2)*randDouble(.85,1)
 		// TODO: Add STAB once we have an enum of Elementals
-		return (int)(((double)2*playerPokémon.getLevel()/25*playerPokémon.getStat(1)
+		return (int)(((double)2*playerPokémon.level/25*playerPokémon.getStat(1)
 				/npcPokémon.getStat(2)*move.getBaseDamage()+2)
 				*ThreadLocalRandom.current().nextDouble(.85, 1));
 	}
--- src/pokémon/PokémonSpeciesCommon.java
+++ src/pokémon/PokémonSpeciesCommon.java
@@ -3,15 +3,19 @@ import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 public class PokémonSpeciesCommon {
 	public Scanner console = new Scanner(System.in);
-	private short pkmnID;
-	private short[] statEVs = new short[6]; // will NEVER be greater than 32767
-	private short[] statIVs = new short[6]; // will NEVER be greater than 32767
-	private short[] stats = new short[6];
+	public short dexID; // will NEVER be greater than 32767
+	/*
+	 * The arrays of shorts stat* are 
+	 */
+	private short[] statEVs = new short[6];
+	private short[] statIVs = new short[6];
+	public short[] stats = new short[6];
 	private boolean IVsBuilt;
-	private boolean[] nvStatusCond = new boolean[7];
-	private short level; // will NEVER be greater than 32767
-	private int exp;
-	// FIXME: add elemental enum, then we can TODO primary/secondary elementals
+	public boolean[] nvStatusCond = new boolean[7];
+	public short level; // will NEVER be greater than 32767
+	public int exp;
+	public Elemental type;
+	// TODO lookup table
 	public void setRandomIVs() {
 		if(IVsBuilt) throw new IllegalArgumentException("IVs already set");
 		for(int i : statIVs) statIVs[i]=(short)ThreadLocalRandom.current().nextInt(0,32);
@@ -23,10 +27,13 @@ public class PokémonSpeciesCommon {
 	public boolean[] getNvStatusCond() {
 		return nvStatusCond;
 	}
-	public void setNvStatusCond(boolean[] nvStatusCond) {
-		this.nvStatusCond = nvStatusCond;
+	public void setNvStatusCond(boolean nvStatusCond, short index) {
+		this.nvStatusCond[index] = nvStatusCond;
 	}
-	public short getLevel() {
-		return level;
+	public short[] yieldEVs(short[] evsGained) {
+		for(int i : statEVs) {
+			this.statEVs[i] += evsGained[i];
+		}
+		return statEVs;	
 	}
 }
