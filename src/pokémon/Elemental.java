64c03e3007a11474d35046ca169f5a6a5d0ab722
116060088 <116060088@ECH-STU-05-21C.SSFSD.LOCAL>
2015-10-02 08:36:35 -0700

    type a pokemon can have

--- src/pokémon/Elemental.java
+++ src/pokémon/Elemental.java
@@ -1,6 +1,6 @@
 package pokémon;
 
-public enum Elemental {
+public enum Elemental { // types a Pokémon can have
 	NORMAL, FIGHTING, FLYING, POISON, GROUND, ROCK, BUG,
 	FIRE, WATER, GRASS, ELECTRIC, PSYCHIC, ICE, DRAGON,
 	GHOST, DARK, STEEL, FAIRY
