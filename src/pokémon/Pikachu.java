560b49297a5063f331ee63c308fa77b94ec34385
116060088 <alt.people.davidcalman@gmail.com>
2016-02-08 09:07:41 -0800

    Pikachu

---
+++ src/pokémon/Pikachu.java
@@ -0,0 +1,7 @@
+package pokémon;
+
+public class Pikachu extends PokémonSpeciesCommon {
+	public Pikachu(int[] stat, int level) {
+		super(25, stat, level, Elemental.ELECTRIC, null);
+	}
+}
--- src/pokémon/PokémonSpeciesCommon.java
+++ src/pokémon/PokémonSpeciesCommon.java
@@ -4,6 +4,36 @@ import java.util.concurrent.ThreadLocalRandom;
 public class PokémonSpeciesCommon {
 	public Scanner console = new Scanner(System.in);
 	private int dexID; // will NEVER be greater than 32767
+	public boolean isConfused() {
+		return isConfused;
+	}
+	public void setConfused(boolean isConfused) {
+		this.isConfused = isConfused;
+	}
+	public int getExp() {
+		return exp;
+	}
+	public void setExp(int exp) {
+		this.exp = exp;
+	}
+	public int getDexID() {
+		return dexID;
+	}
+	public int[] getStats() {
+		return stats;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public Elemental getPrimaryElemental() {
+		return primaryElemental;
+	}
+	public Elemental getSecondaryElemental() {
+		return secondaryElemental;
+	}
+	public void setNvStatusCond(boolean[] nvStatusCond) {
+		this.nvStatusCond = nvStatusCond;
+	}
 	/*
 	 * The arrays of ints stat* are ordered as follows:
 	 * HP, Attack, Defense, Sp.Atk, Sp.Def, and Speed.
@@ -12,6 +42,7 @@ public class PokémonSpeciesCommon {
 	private int[] statIVs = new int[6];
 	private int[] stats = new int[6];
 	private boolean ivsBuilt;
+	// Faint, Poison, Paralyze, Sleep, Burn, Freeze, BadPoison
 	private boolean[] nvStatusCond = new boolean[7];
 	private boolean isConfused;
 	private int level; // will NEVER be greater than 32767
@@ -42,11 +73,11 @@ public class PokémonSpeciesCommon {
 		}
 		return statEVs;	
 	}
-	public PokémonSpeciesCommon(int dexID, int[] stat, int lvl, int xp, Elemental pElemental, Elemental sElemental) {
+	public PokémonSpeciesCommon(int dexID, int[] stat, int lvl, Elemental pElemental, Elemental sElemental) {
 		this.dexID = dexID;
 		stats = stat;
 		level = lvl;
-		exp = xp;
+		exp = 0;
 		primaryElemental = pElemental;
 		secondaryElemental = sElemental;
 		setRandomIVs();
