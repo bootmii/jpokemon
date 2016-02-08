37199f3b18a8a7c73d90c1aed88f18c36e246e36
116060088 <alt.people.davidcalman@gmail.com>
2016-02-08 08:58:28 -0800

    flesh out PokémonSpeciesCommon class

--- src/pokémon/PokémonSpeciesCommon.java
+++ src/pokémon/PokémonSpeciesCommon.java
@@ -3,42 +3,52 @@ import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 public class PokémonSpeciesCommon {
 	public Scanner console = new Scanner(System.in);
-	public short dexID; // will NEVER be greater than 32767
+	private int dexID; // will NEVER be greater than 32767
 	/*
-	 * The arrays of shorts stat* are ordered as follows:
+	 * The arrays of ints stat* are ordered as follows:
 	 * HP, Attack, Defense, Sp.Atk, Sp.Def, and Speed.
 	 */
-	private short[] statEVs = new short[6];
-	private short[] statIVs = new short[6];
-	public short[] stats = new short[6];
-	private boolean IVsBuilt;
-	public boolean[] nvStatusCond = new boolean[7];
-	public short level; // will NEVER be greater than 32767
-	public int exp;
-	public Elemental primaryElemental;
-	public Elemental secondaryElemental;
+	private int[] statEVs = new int[6];
+	private int[] statIVs = new int[6];
+	private int[] stats = new int[6];
+	private boolean ivsBuilt;
+	private boolean[] nvStatusCond = new boolean[7];
+	private boolean isConfused;
+	private int level; // will NEVER be greater than 32767
+	private int exp;
+	private Elemental primaryElemental;
+	private Elemental secondaryElemental;
 	// TODO lookup table
 	public void setRandomIVs() {
-		if(IVsBuilt) throw new IllegalArgumentException("IVs already set");
+		if(ivsBuilt) throw new IllegalArgumentException("IVs already set");
 		// set 6 random ivs from 0 to 32
-		for(int i : statIVs) statIVs[i]=(short)ThreadLocalRandom.current().nextInt(0,32);
-		IVsBuilt = true;
+		for(int i : statIVs) statIVs[i]=(int)ThreadLocalRandom.current().nextInt(0,32);
+		ivsBuilt = true;
 		
 	}
-	public short getStat(int index) {
+	public int getStat(int index) {
 		return this.stats[index];
 	}
 	public boolean[] getNvStatusCond() {
 		return nvStatusCond;
 	}
-	public void setNvStatusCond(boolean nvStatusCond, short index) {
+	public void setNvStatusCond(boolean nvStatusCond, int index) {
 		this.nvStatusCond[index] = nvStatusCond;
 	}
-	public short[] yieldEVs(short[] evsGained) {
+	public int[] yieldEVs(int[] evsGained) {
 		for(int i : statEVs) {
-			// increment evs
+			// increment EVs
 			this.statEVs[i] += evsGained[i];
 		}
 		return statEVs;	
+	}
+	public PokémonSpeciesCommon(int dexID, int[] stat, int lvl, int xp, Elemental pElemental, Elemental sElemental) {
+		this.dexID = dexID;
+		stats = stat;
+		level = lvl;
+		exp = xp;
+		primaryElemental = pElemental;
+		secondaryElemental = sElemental;
+		setRandomIVs();
 	}
 }
